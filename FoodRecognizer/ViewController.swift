a28d8e4812dd31836b811c874c537d057c8e0cba
Adnan Ahmed <adnan_a96@hotmail.com>
2019-05-29 02:27:50 -0400

    Initial Commit

---
+++ FoodRecognizer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodRecognizer
+//
+//  Created by Adnan Ahmed on 2019-05-29.
+//  Copyright © 2019 Adnan Ahmed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
